52dc270e2b2469f5fffc0e96bf1adeea53d8d41d
Ricardo Ferreira <ricardo.ferreira@soprasteria.com>
2021-10-25 23:08:03 +0200

    como criar um fullscreen dialog no android

---
+++ app/src/main/java/com/progdeelite/dca/full_screen_dialog/FullscreenAlertDialog.kt
@@ -0,0 +1,52 @@
+package com.progdeelite.dca.full_screen_dialog
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.DialogFragment
+import com.progdeelite.dca.R
+import com.progdeelite.dca.binding.viewBinding
+import com.progdeelite.dca.databinding.DialogAlertFullscreenBinding
+import com.progdeelite.dca.util.setVisible
+
+class FullscreenAlertDialog(
+    val title: String = "",
+    val message: String = "",
+    val positiveLabel: String = "",
+    val positiveAction: () -> Unit = {},
+    val cancelLabel: String? = null,
+    val cancelAction: () -> Unit = {},
+    val dismissAction: () -> Unit = {},
+) : DialogFragment(R.layout.dialog_alert_fullscreen) {
+
+    private val binding by viewBinding(DialogAlertFullscreenBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            header.text = title
+            body.text = message
+            nextButton.text = positiveLabel
+            nextButton.setOnClickListener {
+                dismiss()
+                positiveAction()
+            }
+            cancelButton.setVisible(cancelLabel != null)
+            cancelButton.text = cancelLabel
+            cancelButton.setOnClickListener {
+                dismiss()
+                cancelAction()
+            }
+        }
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        dismissAction()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): android.app.Dialog {
+        setStyle(STYLE_NO_FRAME, R.style.Theme_DesafiosComunsAndroid_Fullscreen)
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/progdeelite/dca/full_screen_dialog/FullscreenDialogFragment.kt
@@ -0,0 +1,33 @@
+package com.progdeelite.dca.full_screen_dialog
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.progdeelite.dca.R
+import com.progdeelite.dca.binding.viewBinding
+import com.progdeelite.dca.databinding.FragmentFullscreenDialogBinding
+import com.progdeelite.dca.util.showFullscreenAlertDialog
+
+
+// 1) Definir fullscreen dialog e layout
+// 2) Definir style fullscreen
+// 3) Criar extensão para re-utilizar onde quiser
+// 4) Usar Dialog de Tela Cheia na prática
+
+class FullscreenDialogFragment : Fragment(R.layout.fragment_fullscreen_dialog) {
+
+    private val binding by viewBinding(FragmentFullscreenDialogBinding::bind)
+
+    @SuppressLint("InflateParams")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.dialogBtn.setOnClickListener {
+            showFullscreenAlertDialog(
+                "JÁ É INSCRITO?",
+                "RAPAZ, TAIS PERDENDO UM CONTEÚDO ARRETADO VISSE!!"
+            )
+        }
+    }
+}
